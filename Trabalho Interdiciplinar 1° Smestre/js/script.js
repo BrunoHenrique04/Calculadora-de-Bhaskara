2a458ace1f978cf22be2711315696e990ac34946
Androww <87768787+AndrowDev@users.noreply.github.com>
2023-11-24 15:42:44 -0300

    feat: melhor organização
    melhor organização dos codigos: "index.html, calculadora.html, style.css, style2.css" ajuste de formatação texto lousa "script.js"  linha 79+

--- Trabalho Interdiciplinar 1° Smestre/js/script.js
+++ Trabalho Interdiciplinar 1° Smestre/js/script.js
@@ -1,44 +1,42 @@
-const $a = document.querySelector('#aValue')
-const $b = document.querySelector('#bValue')
-const $c = document.querySelector('#cValue')
-const $x1 = document.querySelector('#x1Resp')
-const $x2 = document.querySelector('#x2Resp')
+const x1resp = document.querySelector('#x1Resp')
+const x2resp = document.querySelector('#x2Resp')
 const expressao = document.querySelector('#expArit'); 
 const page2 = document.querySelector('#page2')
 const calcBtn = document.querySelector('#calcBtn');
-const $textoLousa = document.querySelector('#textoLousa');
-const $x1Pos = document.querySelector('#x1Pos');
-const $x2Pos = document.querySelector('#x2Pos');
-const $xResp = document.querySelector('#xResp');
+const textoLousa = document.querySelector('#textoLousa');
+const x1Pos = document.querySelector('#x1Pos');
+const x2Pos = document.querySelector('#x2Pos');
+const xResp = document.querySelector('#xResp');
 
 
 calcBtn.addEventListener('click', bhaskara)
 
 expressao.addEventListener('click', () => {
-    console.log(page2);
+    console.log("scroll");
 
     // Obtém a posição da div em relação à janela
     let posicaoDiv = page2.getBoundingClientRect().y;
 
     // Rola até a div, considerando a posição atual
     window.scrollBy({ 
-        top: posicaoDiv - 100, // Ajusta para cima 20 pixels
-        behavior: 'smooth'
+        top: posicaoDiv - 100, // Ajusta para cima 100 pixels
+        behavior: 'smooth' //suave
     });
 })
 
 
 function bhaskara() {
-    document.getElementById('page2')
-    document.getElementById('louusa_texto')
-    document.getElementById('info').innerHTML = "Informações";
-    $x1.innerHTML = "X1 = 0";
-    $x2.innerHTML = "X2 = 0"; 
-    console.log("bhask function executada");
+    const a = document.querySelector('#aValue').value
+    const b = document.querySelector('#bValue').value
+    const c = document.querySelector('#cValue').value
 
-    const a = $a.value;
-    const b = $b.value;
-    const c = $c.value;
+    document.getElementById('info').innerHTML = "Informações";
+    x1resp.innerHTML = "X1 = 0";
+    x2resp.innerHTML = "X2 = 0";
+    textoLousa.innerHTML = 'EXECUTE UMA CALCULO PARA TER SEU PASSO A PASSO'
+    x1Pos.innerHTML = ''
+    x2Pos.innerHTML = ''
+
     if( !a || !b || !c ) {
         // alert('inserir o valor de A, B, C')
          document.getElementById("info").innerHTML = 'inserir o valor de A, B, C.'
@@ -46,6 +44,7 @@ function bhaskara() {
      } else if ( a == 0) {
        // alert('O valor de A, deve ser diferente de 0')
         document.getElementById("info").innerHTML = 'O valor de A, deve ser diferente de 0.'
+        return
     }
      
     const  delta = b * b -4 * a * c;
@@ -53,53 +52,60 @@ function bhaskara() {
      if ( delta < 0 ) {
        // alert('Sem raízes reais')
         document.getElementById("info").innerHTML = 'O delta é negativo. Equação não possui raízes reais.'
-        $textoLousa.innerHTML = `
-        \u0394 = B^2 -4 * A * C. <br/>
-        \u0394 = ${b}^2 -4 * ${a} * ${c}. <br/>
-        \u0394 = ${b * b} -4 * ${a} * ${c}. <br/>
-        \u0394 = ${b * b} -4 * ${a * c}. <br/>
-        \u0394 = ${b * b} ${-4 * (a * c)}. <br/>
-        \u0394 = ${(b * b) -4 * (a * c)}. <br/>
+        textoLousa.innerHTML = `
+        Δ = B² -4 * A * C. <br/>
+        Δ = ${b}² -4 * ${a} * ${c}. <br/>
+        Δ = ${b * b} -4 * ${a} * ${c}. <br/>
+        Δ = ${b * b} -4 * ${a * c}. <br/>
+        Δ = ${b * b} ${-4 * (a * c)}. <br/>
+        Δ = ${(b * b) -4 * (a * c)}. <br/>
         ` 
     }
     else {
-        const raizDelta = Math.sqrt(delta)
+        let raizDelta = Math.sqrt(delta)
+         raizDelta = parseFloat(raizDelta.toFixed(2))
         const x1 = ( -b + Math.sqrt(delta)) / ( 2 *a )
         const x2 = ( -b - Math.sqrt(delta)) / ( 2 *a )
-        $textoLousa.innerHTML = `
+        textoLousa.innerHTML = `
         CALCULO DO DELTA: <br/>
-        \u0394 = B^2 -4 * A * C. <br/>
-        \u0394 = ${b}^2 -4 * ${a} * ${c}. <br/>
-        \u0394 = ${b * b} -4 * ${a} * ${c}. <br/>
-        \u0394 = ${b * b} -4 * ${a * c}. <br/>
-        \u0394 = ${b * b} ${-4 * (a * c)}. <br/>
-        \u0394 = ${(b * b) -4 * (a * c)}. <br/>
+        Δ = B² -4 * A * C. <br/>
+        Δ = ${b}² -4 * ${a} * ${c}. <br/>
+        Δ = ${b * b} -4 * ${a} * ${c}. <br/>
+        Δ = ${b * b} -4 * ${a * c}. <br/>
+        Δ = ${b * b} ${-4 * (a * c)}. <br/>
+        Δ = ${(b * b) -4 * (a * c)}. <br/>
         <hr>
         CALCULO DA BASKHARA: <br/>
-        X = -B &#177 &#8730\u0394 / 2 * A <br/>
-        X = -${b} &#177 &#8730 ${delta} / 2 * ${a} <br/>
-        X = ${-b} &#177 ${raizDelta} / 2 * ${a} <br/>
-        X = ${-b} &#177 ${raizDelta} / ${2 * a} <br/>  `
+        X = <span class="fracao"> -B ± √Δ <hr> 2 * A </span> <br/>
+        ${b < 0 ? `
+            X = <span class="fracao"> -(${b}) ± √${delta} <hr> 2 * ${a} </span> <br/>
+            X = <span class="fracao"> -(${b}) ± √${delta} <hr> 2 * ${a} </span><br/>
+        ` : `
+            X = <span class="fracao"> -${b} ± √${delta} <hr> 2 * ${a} </span> <br/>
+            X = <span class="fracao"> -${b} ± √${delta} <hr> 2 * ${a} </span><br/>
+        `}
+        
+        X = <span class="fracao"> ${-b} ± ${raizDelta} <hr> ${2 * a} </span><br/>  `
 
-        $x1Pos.innerHTML = `
-        X1 = ${-b} + ${raizDelta} / ${2 * a} <br/>
-        X1 = ${-b + raizDelta} / ${2 * a} <br/>
-        X1 = ${(-b + raizDelta) / (2 * a)} <br/>`
+        x1Pos.innerHTML = `
+        X1 = <span class="fracao"> ${-b} + ${raizDelta} <hr> ${2 * a} </span><br/>
+        X1 = <span class="fracao"> ${-b + raizDelta} <hr> ${2 * a} </span><br/>
+        X1 = <span class="fracao"> ${x1.toFixed(2)} </span><br/>`
 
-        $x2Pos.innerHTML = `
-        X2 = ${-b} - ${raizDelta} / ${2 * a} <br/>
-        X2 = ${-b - raizDelta} / ${2 * a} <br/>
-        X2 = ${(-b - raizDelta) / (2 * a)} <br/>`
+        x2Pos.innerHTML = `
+        X2 = <span class="fracao">  ${-b} - ${raizDelta} <hr> ${2 * a} </span><br/>
+        X2 = <span class="fracao"> ${-b - raizDelta} <hr> ${2 * a} </span><br/>
+        X2 = <span class="fracao">  ${x2.toFixed(2)} </span><br/>`
 
-        $xResp.innerHTML = `
+        xResp.innerHTML = `
         <br/>
         X1 = ${x1.toFixed(2)} <br/>
         X2 = ${x2.toFixed(2)} <br/>
         `
 
-        
-        $x1.innerHTML = "X1 = " + x1.toFixed(2)
-        $x2.innerHTML = "X2 = " + x2.toFixed(2)
+
+         x1resp.innerHTML = "X1 = " + x1.toFixed(2)
+         x2resp.innerHTML = "X2 = " + x2.toFixed(2)
 
     }
 }
@@ -111,11 +117,11 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function exibirCoachMark() {
-    var coachMark = document.getElementById("coachMark");
+    let coachMark = document.getElementById("coachMark");
     coachMark.style.display = "block";
 }
 
 function fecharCoachMark() {
-    var coachMark = document.getElementById("coachMark");
+    let coachMark = document.getElementById("coachMark");
     coachMark.style.display = "none";
 }
