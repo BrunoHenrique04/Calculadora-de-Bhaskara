f2527674dbe8948725f776fecfe5987f9e428aac
Androww <87768787+AndrowDev@users.noreply.github.com>
2023-11-23 22:24:01 -0300

    feat: adiciona exibição na lousa "script.js"
    após efetuar o calculo sera exibido o passo a passo an lousa.

--- Trabalho Interdiciplinar 1° Smestre/js/script.js
+++ Trabalho Interdiciplinar 1° Smestre/js/script.js
@@ -3,8 +3,37 @@ const $b = document.querySelector('#bValue')
 const $c = document.querySelector('#cValue')
 const $x1 = document.querySelector('#x1Resp')
 const $x2 = document.querySelector('#x2Resp')
- 
+const expressao = document.querySelector('#expArit'); 
+const page2 = document.querySelector('#page2')
+const calcBtn = document.querySelector('#calcBtn');
+const $textoLousa = document.querySelector('#textoLousa');
+const $x1Pos = document.querySelector('#x1Pos');
+const $x2Pos = document.querySelector('#x2Pos');
+const $xResp = document.querySelector('#xResp');
+
+
+calcBtn.addEventListener('click', bhaskara)
+
+expressao.addEventListener('click', () => {
+    console.log(page2);
+
+    // Obtém a posição da div em relação à janela
+    let posicaoDiv = page2.getBoundingClientRect().y;
+
+    // Rola até a div, considerando a posição atual
+    window.scrollBy({ 
+        top: posicaoDiv - 100, // Ajusta para cima 20 pixels
+        behavior: 'smooth'
+    });
+})
+
+
 function bhaskara() {
+    document.getElementById('page2')
+    document.getElementById('louusa_texto')
+    document.getElementById('info').innerHTML = "Informações";
+    $x1.innerHTML = "X1 = 0";
+    $x2.innerHTML = "X2 = 0"; 
     console.log("bhask function executada");
 
     const a = $a.value;
@@ -23,13 +52,53 @@ function bhaskara() {
      if ( delta < 0 ) {
        // alert('Sem raízes reais')
         document.getElementById("info").innerHTML = 'O delta é negativo. Equação não possui raízes reais.'
+        $textoLousa.innerHTML = `
+        \u0394 = B^2 -4 * A * C. <br/>
+        \u0394 = ${b}^2 -4 * ${a} * ${c}. <br/>
+        \u0394 = ${b * b} -4 * ${a} * ${c}. <br/>
+        \u0394 = ${b * b} -4 * ${a * c}. <br/>
+        \u0394 = ${b * b} ${-4 * (a * c)}. <br/>
+        \u0394 = ${(b * b) -4 * (a * c)}. <br/>
+        ` 
     }
     else {
+        const raizDelta = Math.sqrt(delta)
         const x1 = ( -b + Math.sqrt(delta)) / ( 2 *a )
         const x2 = ( -b - Math.sqrt(delta)) / ( 2 *a )
+        $textoLousa.innerHTML = `
+        CALCULO DO DELTA: <br/>
+        \u0394 = B^2 -4 * A * C. <br/>
+        \u0394 = ${b}^2 -4 * ${a} * ${c}. <br/>
+        \u0394 = ${b * b} -4 * ${a} * ${c}. <br/>
+        \u0394 = ${b * b} -4 * ${a * c}. <br/>
+        \u0394 = ${b * b} ${-4 * (a * c)}. <br/>
+        \u0394 = ${(b * b) -4 * (a * c)}. <br/>
+        <hr>
+        CALCULO DA BASKHARA: <br/>
+        X = -B &#177 &#8730\u0394 / 2 * A <br/>
+        X = -${b} &#177 &#8730 ${delta} / 2 * ${a} <br/>
+        X = ${-b} &#177 ${raizDelta} / 2 * ${a} <br/>
+        X = ${-b} &#177 ${raizDelta} / ${2 * a} <br/>  `
 
-        $x1.innerHTML = "X1 = " + x1
-        $x2.innerHTML = "X2 = " + x2
+        $x1Pos.innerHTML = `
+        X1 = ${-b} + ${raizDelta} / ${2 * a} <br/>
+        X1 = ${-b + raizDelta} / ${2 * a} <br/>
+        X1 = ${(-b + raizDelta) / (2 * a)} <br/>`
+
+        $x2Pos.innerHTML = `
+        X2 = ${-b} - ${raizDelta} / ${2 * a} <br/>
+        X2 = ${-b - raizDelta} / ${2 * a} <br/>
+        X2 = ${(-b - raizDelta) / (2 * a)} <br/>`
+
+        $xResp.innerHTML = `
+        <br/>
+        X1 = ${x1.toFixed(2)} <br/>
+        X2 = ${x2.toFixed(2)} <br/>
+        `
+
+        
+        $x1.innerHTML = "X1 = " + x1.toFixed(2)
+        $x2.innerHTML = "X2 = " + x2.toFixed(2)
 
     }
 }
